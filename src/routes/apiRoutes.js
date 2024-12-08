a860119b8da64087bd934f9e5550f1d55faa2126
Quenteon <quanteonquantumelysium@gmail.com>
2024-12-08 13:28:57 +0000

    added expres service

---
+++ src/routes/apiRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    // The return value is configured.
+    // The date and time are prepared ISO format.
+    var value = {
+        dateTime: new Date().toISOString(),
+    }
+
+    // The successful answer is transmitted and data is sent.
+    var result = res.status(200).json(value)
+
+    // response
+    return result
+});
+
+module.exports = router;
